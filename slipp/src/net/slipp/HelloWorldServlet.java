322b56515bcf09fc03486a76c571da53a67cd4de
Seunghan Rho <rrho8822@gmail.com>
2015-07-19 19:20:01 -0400

    first commit

---
+++ slipp/src/net/slipp/HelloWorldServlet.java
@@ -0,0 +1,19 @@
+package net.slipp;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet(name="HelloWorld", urlPatterns={"/helloworld", "/hello", "/hello/world"})
+public class HelloWorldServlet extends HttpServlet {	
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp)
+			throws ServletException, IOException {
+		String name = req.getParameter("name");
+		resp.getWriter().print(name + " Hello World!");
+	}
+}
